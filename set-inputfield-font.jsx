9cad2dde55e27bd006b9e4029ad7e98806030740
Gerald Singelmann <37876569+gsingelmann@users.noreply.github.com>
2021-12-26 10:20:20 +0100

    Add files via upload

---
+++ set-inputfield-font.jsx
@@ -0,0 +1,25 @@
+﻿main();
+
+function main() {
+
+  if ( ! app.documents.length ) return null;
+
+  var doc = app.activeDocument;
+  var d_family = doc.textDefaults.appliedFont.fontFamily;
+  var d_style = doc.textDefaults.fontStyle;
+  var d_size = doc.textDefaults.pointSize;
+
+  if ( app.selection.length ) {
+    for ( var n = 0; n < app.selection.length ; n++ ) {
+      if ( app.selection[n].constructor.name == "TextBox" ) {
+        app.selection[n].appliedFont = d_family;
+        app.selection[n].fontStyle = d_style;
+        app.selection[n].fontSize = d_size;
+      }
+    }
+  } else {
+    doc.textBoxes.everyItem().appliedFont = d_family;
+    doc.textBoxes.everyItem().fontStyle = d_style;
+    doc.textBoxes.everyItem().fontSize = d_size;
+  }
+}
